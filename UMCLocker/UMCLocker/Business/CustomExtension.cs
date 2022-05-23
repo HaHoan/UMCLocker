9e39076cc29aa307dd9cccae88befddfe25318f8
u34811 <u34811@UMC-C998>
2022-05-23 15:07:41 +0700

    Sửa sync data bị lỗi

---
+++ UMCLocker/UMCLocker/Business/CustomExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMCLocker.Business
+{
+    public static class CustomExtension
+    {
+        public static string ToCompleteString(this string str)
+        {
+            if(str.Length < 5)
+            {
+                var needAddZero = 5 - str.Length;
+                var zero = "";
+                for(int i = 0; i < needAddZero; i++)
+                {
+                    zero += "0";
+                }
+                str = zero + str;
+            }
+            return str;
+        }
+    }
+}
